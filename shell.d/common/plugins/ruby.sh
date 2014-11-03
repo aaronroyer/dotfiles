881a6ab1545a6a1646243c72c5d832c2a37957e5
Aaron Royer <aaronroyer@gmail.com>
2014-11-03 12:06:57 -0800

    Try a solution for smart execution of ruby stuff

--- shell.d/common/plugins/ruby.sh
+++ shell.d/common/plugins/ruby.sh
@@ -1,17 +1,31 @@
-alias gsr='gem search --remote'
-
 alias be='bundle exec'
-alias bi='bundle install'
 
 alias z=zeus
 alias zs='zeus server'
 alias zc='zeus console'
 alias zt='zeus spec'
 
-function r() {
-  if [ -x bin/rake ]; then
-    bin/rake $@
-  elif [ -a Gemfile ]; then
-    bundle exec rake $@
+function ruby_exec() {
+  local command=$1
+  [ -z "$command" ] && { echo "$0 must be given a command name and any arguments" >&2; return 1; }
+  if [ -x "bin/$command" ]; then
+    if [ -d .git/safe ]; then
+      shift
+      $command "$@"
+      return
+    else
+      echo "$0: .git/safe directory does not exist - not executing binstub"
+    fi
+  fi
+
+  if [ "$command" != bundle -a -f Gemfile ] && command -v bundle >/dev/null 2>&1; then
+    shift
+    bundle exec $command "$@"
+  else
+    "$@"
   fi
 }
+
+for command in rake rails rspec; do
+  alias $command="ruby_exec $command"
+done
