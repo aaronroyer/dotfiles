cb0a6d4fc664697c20f4e1154aded13133bcbb0c
Aaron Royer <aaronroyer@gmail.com>
2015-02-15 12:53:24 -0800

    Add npm completion

--- shell.d/zsh/completion.zsh
+++ shell.d/zsh/completion.zsh
@@ -3,3 +3,5 @@ compinit # Completions in ~/.zsh/functions
 
 # Case-insensitive (uppercase from lowercase) completion
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+
+which npm &> /dev/null && source <(npm completion)
